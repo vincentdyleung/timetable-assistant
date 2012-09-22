bf8c69cca21e62d3bb05ff0d70a94e7a19409310
Vincent Leung <vincentleung1991@gmail.com>
2012-09-22 14:01:05 -0400

    added rails code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimetableAssistant::Application.config.secret_token = '625f963dad9048ba28920eecba098f976901d86a138c751e1ef79927d2730463a759f1b0e6924e127dd4740090e6b06002a259ba6ff6eed654f88f19079b6ce9'
